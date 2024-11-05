90d86ed1c8c7cec8901ebf04ead319d265b04fa8
aleruizrg <aleruiz@alumno.unlam.edu.ar>
2024-11-04 22:44:00 -0300

    subida inicial de proyecto
    subida de proyecto mvc para la prediccion de imagenes con Inteligencia Artificial y Machine Learning

---
+++ MLNetMVC/MLNetMVC.Data/EF/TranferLearningMlContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MLNetMVC.Data.EF;
+
+public partial class TranferLearningMlContext : DbContext
+{
+    public TranferLearningMlContext()
+    {
+    }
+
+    public TranferLearningMlContext(DbContextOptions<TranferLearningMlContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ImageDatum> ImageData { get; set; }
+
+    public virtual DbSet<ImagePrediction> ImagePredictions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=TranferLearningML;Trusted_Connection=True;Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ImageDatum>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ImageDat__3214EC0782DA8E1E");
+        });
+
+        modelBuilder.Entity<ImagePrediction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ImagePre__3214EC079411B650");
+
+            entity.ToTable("ImagePrediction");
+
+            entity.HasOne(d => d.ImageData).WithMany(p => p.ImagePredictions)
+                .HasForeignKey(d => d.ImageDataId)
+                .HasConstraintName("FK__ImagePred__Image__5CD6CB2B");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MLNetMVC/MLNetMVC.Web/Controllers/PredictionController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MLNetMVC.Data.EF;
+using MLNetMVC.Logica;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace MLNetMVC.Web.Controllers
+{
+    public class PredictionController : Controller
+    {
+        private readonly IMLNetLogica _mlNetLogica;
+
+        public PredictionController(IMLNetLogica mlNetLogica)
+        {
+            _mlNetLogica = mlNetLogica;
+        }
+
+        public async Task<ActionResult> AnalizarImagen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AnalizarImagen(IFormFile imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                var modelo = _mlNetLogica.GenerateModel();
+                var imagenAnalizada = await _mlNetLogica.ClassifySingleImage(imageFile, modelo);
+                return View(imagenAnalizada);
+            }
+
+            return View(imageFile);
+        }
+
+        [HttpPost]
+        public ActionResult AgregarElemento(string ImagePath, float Score, string PredictedLabelValue)
+        {
+            if (ModelState.IsValid) {
+                
+
+                _mlNetLogica.AgregarElemento(ImagePath,Score,PredictedLabelValue);
+
+                return RedirectToAction("AnalizarImagen");
+            }
+            return RedirectToAction("AnalizarImagen");
+        }
+
+        [HttpPost]
+        public ActionResult CorregirElementoView(string ImagePath)
+        {
+            ViewBag.ImagePath = ImagePath;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CorregirElemento(string ImagePath,string label)
+        {
+            _mlNetLogica.CorregirElemento(ImagePath, label);
+
+            return RedirectToAction("AnalizarImagen");
+        }
+    }
+}
