3761335c8e7cfaf9c1008c0361f2d1d2b98330c4
marianoluisvargas <marianoluisvargas@gmail.com>
2024-11-17 12:49:57 -0300

    feat(MLNetProyecto.Web.Services): agregado de httpclient

--- MLNetProyecto/MLNetProyecto.Web/Controllers/HomeController.cs
+++ MLNetProyecto/MLNetProyecto.Web/Controllers/HomeController.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MLNetProyecto.Web.Areas.Identity.Data;
+using MLNetProyecto.Web.Services;
 using MLNetProyecto.Web.Models;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MLNetProyecto.Web.Controllers
 {
@@ -12,16 +14,32 @@ namespace MLNetProyecto.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<User> _userManager;
+        private readonly ApiService _apiService;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, ApiService apiService)
         {
             _logger = logger;
             _userManager = userManager;
+            _apiService = apiService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewData["UserId"] = _userManager.GetUserId(this.User);
+            string userId = _userManager.GetUserId(this.User);
+            ViewData["UserId"] = userId;
+
+            try
+            {
+                var efficiencyData = await _apiService.GetAsync<EfficiencyResponse>("page_1_current_year/search_efficiency");
+
+                ViewData["EfficiencyData"] = efficiencyData.Efficiency;
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError($"Error al llamar a la API: {ex.Message}");
+                ViewData["EfficiencyData"] = "Error al obtener datos de eficiencia.";
+            }
+
             return View();
         }
 
@@ -36,4 +54,6 @@ namespace MLNetProyecto.Web.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+
 }
---
+++ MLNetProyecto/MLNetProyecto.Web/Services/ApiService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MLNetProyecto.Web.Services;
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("http://127.0.0.1:8000/"); // Reemplazar con la api
+    }
+
+    public async Task<T> GetAsync<T>(string endpoint)
+    {
+        var response = await _httpClient.GetAsync(endpoint);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(jsonResponse);
+        }
+
+        throw new HttpRequestException($"Error en la petición: {response.StatusCode}");
+    }
+
+    public async Task<T> PostAsync<T>(string endpoint, object data)
+    {
+        var jsonContent = JsonSerializer.Serialize(data);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(endpoint, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(jsonResponse);
+        }
+
+        // Manejo de errores
+        throw new HttpRequestException($"Error en la petición: {response.StatusCode}");
+    }
+}
